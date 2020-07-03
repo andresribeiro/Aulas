348110079e0679b05ed67ec0d7a5b3a303a9dbd3
André Ribeiro <andresribeiro@tutanota.com>
2020-07-03 18:20:51 -0300

    Delete IP Tracking

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,21 +3,11 @@ import { View, Text, StyleSheet } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
 import { MaterialIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import axios from 'axios'
 
 export default function All() {
 
   const navigation = useNavigation()
   
-  axios.get('https://api.ipify.org').then(resp => {
-
-    const ip = resp.data;
-    axios.get(`https://aulas-app-backend.herokuapp.com/${ip}`).then(response => {
-
-      console.log(response.data);
-    })
-  })
-
   return(
     <View style={styles.container} >
     <MaterialIcons name="today" size={65} color="#7159C1" />
